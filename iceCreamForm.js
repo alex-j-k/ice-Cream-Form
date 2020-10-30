e989ed73eb1bbb4f3b867a5569ae39a26784928e
alex-j-k <63852006+alex-j-k@users.noreply.github.com>
2020-10-31 00:07:30 +1000

    finished

--- iceCreamForm.js
+++ iceCreamForm.js
@@ -1,16 +1,52 @@
 // change ice cream cone photo to match flavour
-const radiobuttonchoc = document.querySelector('#chocolate');
-
+const submitButton = document.querySelector('#submit');
+const submit = document.querySelector('#submitt');
 
 
 function changeCone() {
-    if (document.getElementById('Chocolate').checked == true) {
-        document.body.style.backgroundImage = "url('choc.jpeg')"
-    } else { 
-        document.getElementById('heading').style.backgroundColor = 'blue'
+    if (document.getElementById('Chocolate').checked == true) {  
+        document.body.style.backgroundImage = "url('choc.jpg')";
 
+    } else if (document.getElementById('Vanilla').checked == true) {  
+            document.body.style.backgroundImage = "url('vanilla.jpg')";
+    } else {
+        document.body.style.backgroundImage = "url('icecream.jpg')";
     }
 };
+
+
+submit.addEventListener('click', e => {
+        e.preventDefault();
+        let name = document.querySelector('#user').value;
+
+        if (document.querySelector('#Chocolate').checked == true) {
+        flavour = document.querySelector('#Chocolate').value
+        } else if (document.querySelector('#Vanilla').checked == true){
+            flavour = document.querySelector('#Vanilla').value
+        } else if (document.querySelector('#Strawberry').checked == true){
+            flavour = document.querySelector('#Strawberry').value
+        } else { alert('Please choose a flavour!')};
+        if (name == '') {name= 'Anoymous'};
+        generatePerson(flavour, name);
+        document.getElementById("form").reset();
+    });
+
+const generatePerson = (flav, name) => {
+    const html = `<li><span >${flav},</span><span> ${name}</span></li>`;
+    document.querySelector('#list').innerHTML += html; 
+    };
+
+
+
+// function changeCone() {
+//     if (document.getElementById('Chocolate').checked == true) {  
+//         document.body.style.backgroundImage = "url('choc.jpg')";
+
+//     } else { 
+//         document.getElementById('heading').style.backgroundColor = 'blue'
+
+//     }
+// };
 
 // function changeCone() {
 //     if (document.getElementById('Chocolate').checked == true) {
